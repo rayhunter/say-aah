b768d4a4f7687c789c7f25d2688be3b6ca3ba9fa
ray hunter <ray.hunter@gmail.com>
2013-05-23 14:23:12 -0700

    last iteration

---
+++ app/favelet.js
@@ -0,0 +1,4 @@
+'use strict';
+// favelet to pass article copy to say-aah
+
+alert('every good boy deserves favor');
--- app/scripts/controllers/analyze.js
+++ app/scripts/controllers/analyze.js
@@ -4,19 +4,21 @@ sayAahApp.controller('AnalyzeCtrl', function( $scope, $http ) {
   $scope.master= {};
   $scope.placeholder= {};
   $scope.request='';
+  $( 'div.current' ).hide();
   
   var config = {
-  	headers:  {
+    headers:  {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Request-Headers': 'origin, content-type',
         'Accept-encoding': 'gzip'
     }
   };
 
-  $scope.update = function(article) {
+  $scope.update = function( article ) {
     $scope.master= angular.copy(article);
     $scope.data = angular.copy(article);
     $.articleText = angular.copy(article);
+    $('div.current').show();
 
     $.ajax({
         url: "http://access.alchemyapi.com/calls/text/TextGetRankedKeywords?apikey=b24e5860c2091260bc776c7e075b9532f5e1a259&outputMode=json&showSourceText=1&text="+$.articleText+"&jsonp=?",
@@ -24,22 +26,25 @@ sayAahApp.controller('AnalyzeCtrl', function( $scope, $http ) {
         data: [{ text: $scope.data }],
         dataType: 'jsonp',
         success: function( response ){
-            $scope.resultTags = JSON.stringify(response);
-            $( 'div#target.tags span' ).text($scope.resultTags);
+            var $keywords = [];
+            for ( var i = 0; i <  response.keywords.length; i++ ) {
+                //console.log( response.keywords[i].text );
+                $keywords.push( ' '+ response.keywords[i].text);
+            }
+            $( 'div#target.tags span' ).text( $keywords );
         },
         error:function(){
             alert( "Error" );
         },
     });
 
-$.ajax({
+    $.ajax({
         url: "http://access.alchemyapi.com/calls/text/TextGetCategory?apikey=b24e5860c2091260bc776c7e075b9532f5e1a259&outputMode=json&showSourceText=1&text="+$.articleText+"&jsonp=?",
         type: 'POST',
         data: [{ text: $scope.data }],
         dataType: 'jsonp',
         success: function( response ){
-            $scope.resultCats = JSON.stringify( response );
-            $('div#target1.categories span').text( $scope.resultCats );
+            $('div#target1.categories span').text( response.category );
         },
         error:function(){
             alert( "Error" );
@@ -48,15 +53,14 @@ $.ajax({
 
     $( 'div#target' ).show( "slow" );
     $( 'div#target1' ).show( "slow" );
-
     return false;
 
   };
  
   $scope.reset = function() {
-    //$scope.article = angular.copy( $scope.master );
     $scope.article = "";
     $scope.master = "";
+    $( 'div.current' ).hide( "slow" );
     $( 'div#target' ).hide( "slow" );
     $( 'div#target1' ).hide( "slow" );
   };
